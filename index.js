c1a03845a93a63e5f782dc0d9a589210dbc0bfd3
tid45 <123549768+tid45@users.noreply.github.com>
2023-09-05 22:51:21 +0900

    fourth test
    test try4

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+
+const rootTwo = ReactDOM.createRoot(document.getElementById('rootPage'));
+rootTwo.render(
+    <App />
+);
+
